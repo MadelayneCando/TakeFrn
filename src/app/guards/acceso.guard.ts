56dddbf627f091f7d2a6ef6bb322c229f02ae0f6
zJostyn <jostyncruzquito@gmail.com>
2024-01-31 02:21:52 -0500

    RegresoNoGurd

--- src/app/guards/acceso.guard.ts
+++ src/app/guards/acceso.guard.ts
@@ -10,20 +10,9 @@ export class accesoGuard implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     const bandera = localStorage.getItem('bandera');
-    const tipo = localStorage.getItem('idtipo');
-
+    
     if (bandera === 'true') {
-      if (tipo === '2') {
-        const allowedRoutesForType2 = ['instructores', 'clases', 'usuarios'];
-        if (allowedRoutesForType2.includes(state.url.split('/')[1])) {
-          return true;
-        } else {
-          this.router.navigate(['/perfil']);
-          return false;
-        }
-      } else {
-        return true;
-      }
+      return true;
     } else {
       this.router.navigate(['/login']);
       return false;
