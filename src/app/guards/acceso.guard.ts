4c6e522399b42c4c015c0c1779fb88cda09a8907
zJostyn <jostyncruzquito@gmail.com>
2024-01-31 23:29:32 -0500

    NuevoCambGuard

--- src/app/guards/acceso.guard.ts
+++ src/app/guards/acceso.guard.ts
@@ -9,10 +9,33 @@ export class accesoGuard implements CanActivate {
   constructor(private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    const bandera = localStorage.getItem('bandera');    
+    const bandera = localStorage.getItem('bandera');
+    const tipo = localStorage.getItem('idtipo');
+    
     if (bandera === 'true') {
-      return true;
+      if (tipo === '1') {
+        // Si bandera es true y tipo es 1, permitir el acceso a ciertas rutas
+        const allowedRoutesForType1 = ['clases', 'instructores', 'perfil', 'usuarios'];
+        if (allowedRoutesForType1.includes(state.url.split('/')[1])) {
+          return true;
+        } else {
+          // Redirigir a otra ruta si intenta acceder a una ruta no permitida para tipo 1
+          this.router.navigate(['/perfil']); // Puedes ajustar la ruta de redirección según tus necesidades
+          return false;
+        }
+      } else {
+        // Si bandera es true y tipo no es 1, permitir el acceso solo a compracreditos y perfil
+        const allowedRoutesForTypeNot1 = ['compracreditos', 'perfil'];
+        if (allowedRoutesForTypeNot1.includes(state.url.split('/')[1])) {
+          return true;
+        } else {
+          // Redirigir a otra ruta si intenta acceder a una ruta no permitida para tipo no 1
+          this.router.navigate(['/perfil']); // Puedes ajustar la ruta de redirección según tus necesidades
+          return false;
+        }
+      }
     } else {
+      // Redirigir a la página de inicio de sesión si bandera no es true
       this.router.navigate(['/login']);
       return false;
     }
