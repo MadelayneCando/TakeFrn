0424b3d248d886f997bd06004417c7ef0513a757
zJostyn <jostyncruzquito@gmail.com>
2024-01-31 21:34:44 -0500

    CambGuard

--- src/app/guards/acceso.guard.ts
+++ src/app/guards/acceso.guard.ts
@@ -10,10 +10,25 @@ export class accesoGuard implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     const bandera = localStorage.getItem('bandera');
-    
+    const tipo = localStorage.getItem('idtipo');
+
     if (bandera === 'true') {
-      return true;
+      if (tipo === '1') {
+        // Si tipo es 1, permitir el acceso solo a estas rutas
+        const allowedRoutesForType2 = ['instructores', 'clases', 'usuarios'];
+        if (allowedRoutesForType2.includes(state.url.split('/')[1])) {
+          return true;
+        } else {
+          // Redirigir a otra ruta si intenta acceder a una ruta no permitida para tipo 2
+          this.router.navigate(['/perfil']);
+          return false;
+        }
+      } else {
+        // Permitir el acceso a todas las rutas si tipo no es 2
+        return true;
+      }
     } else {
+      // Redirigir a la página de inicio de sesión si bandera no es true
       this.router.navigate(['/login']);
       return false;
     }
